218573153edbec26bcfe16498a1c208a5be83a2e
abdul <muyeezabdul7@gmail.com>
2026-02-25 14:30:45 +0530

    conftest updated

--- conftest.py
+++ conftest.py
@@ -82,13 +82,26 @@ def setup(browser, request):
             options.add_argument("--high-dpi-support=1")
             options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
 
+            options.add_argument("--disable-blink-features=AutomationControlled")
+            options.add_argument("--disable-infobars")
+            options.add_argument("--start-maximized")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+
+            options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            options.add_experimental_option("useAutomationExtension", False)
+
         service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
-
+        driver.execute_script(
+            "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+        )
         # explicit headless setup
         if headless:
             driver.set_window_size(1920, 1080)
             options.add_argument("--start-maximized")
+
+
 
     elif browser_name == "firefox":
         options = webdriver.FirefoxOptions()
